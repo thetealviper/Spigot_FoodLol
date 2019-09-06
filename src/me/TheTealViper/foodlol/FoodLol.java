7fa2664909bc17d9b256da6055cf1933d86b1090
TheTealViper <thetealviper@gmail.com>
2019-09-06 13:49:21 -0700

    [1.14.4.a.1] Init push

---
+++ src/me/TheTealViper/foodlol/FoodLol.java
@@ -0,0 +1,406 @@
+package me.TheTealViper.foodlol;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import me.TheTealViper.foodlol.Utils.EnableShit;
+import me.TheTealViper.foodlol.Utils.ItemCreator;
+import me.TheTealViper.foodlol.Utils.PluginFile;
+import me.TheTealViper.foodlol.Utils.Sound;
+
+public class FoodLol extends JavaPlugin implements Listener{
+	Map<String, ItemStack> foodInfo = new HashMap<String, ItemStack>();
+	Map<String, Double> healthInfo = new HashMap<String, Double>();
+	Map<String, Double> hungerInfo = new HashMap<String, Double>();
+	Map<String, ItemStack> returnInfo = new HashMap<String, ItemStack>();
+	Map<String, List<String>> effectsInfo = new HashMap<String, List<String>>();
+	Map<String, List<String>> commandInfo = new HashMap<String, List<String>>();
+	Map<String, Long> cooldownData = new HashMap<String, Long>();
+	boolean overrideMaxHealth = false;
+	PluginFile inGameMadeRecipes = new PluginFile(this, "InGameMadeRecipes.yml");
+	
+	public void onEnable(){
+		EnableShit.handleOnEnable(this, this, "-1");
+		overrideMaxHealth = getConfig().getBoolean("Override_Max_Health");
+		
+		loadRecipes();
+	}
+	
+	public void onDisable(){
+		//Bukkit.getLogger().info(makeColors("FoodLol from TheTealViper shutting down. Bshzzzzzz"));
+	}
+	
+	private void loadRecipes(){
+		//Default config
+		ConfigurationSection allFoodSec = getConfig().getConfigurationSection("Food");
+		for(String foodID : allFoodSec.getKeys(false)){
+			ConfigurationSection foodSec = allFoodSec.getConfigurationSection(foodID);
+			ItemStack foodItem = ItemCreator.createItemFromConfiguration(foodSec);
+			ItemMeta meta = foodItem.getItemMeta();
+			meta.setUnbreakable(true);
+			meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE);
+			foodItem.setItemMeta(meta);
+			foodInfo.put(foodID, foodItem);
+			healthInfo.put(foodID, foodSec.getDouble("health"));
+			hungerInfo.put(foodID, foodSec.getDouble("hunger"));
+			if(foodSec.contains("effects"))
+				effectsInfo.put(foodID, foodSec.getStringList("effects"));
+			if(foodSec.contains("return"))
+				returnInfo.put(foodID, ItemCreator.createItemFromConfiguration(foodSec.getConfigurationSection("return")));
+			if(foodSec.contains("commands"))
+				commandInfo.put(foodID, foodSec.getStringList("commands"));
+			else
+				commandInfo.put(foodID, new ArrayList<String>());
+		}
+		//In game config
+		allFoodSec = inGameMadeRecipes.getConfigurationSection("Food");
+		if(allFoodSec != null){
+			for(String foodID : allFoodSec.getKeys(false)){
+				ConfigurationSection foodSec = allFoodSec.getConfigurationSection(foodID);
+				ItemStack foodItem = ItemCreator.createItemFromConfiguration(foodSec);
+				ItemMeta meta = foodItem.getItemMeta();
+				meta.setUnbreakable(true);
+				meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE);
+				foodItem.setItemMeta(meta);
+				foodInfo.put(foodID, foodItem);
+				healthInfo.put(foodID, foodSec.getDouble("health"));
+				hungerInfo.put(foodID, foodSec.getDouble("hunger"));
+				if(foodSec.contains("effects"))
+					effectsInfo.put(foodID, foodSec.getStringList("effects"));
+				else
+					effectsInfo.put(foodID, new ArrayList<String>());
+				if(foodSec.contains("return"))
+					returnInfo.put(foodID, ItemCreator.createItemFromConfiguration(foodSec.getConfigurationSection("return")));
+				if(foodSec.contains("commands"))
+					commandInfo.put(foodID, foodSec.getStringList("commands"));
+				else
+					commandInfo.put(foodID, new ArrayList<String>());
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onInteract(PlayerInteractEvent e){
+		if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+			if(onRightClick(e.getPlayer()))
+				e.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onInteract(PlayerInteractAtEntityEvent e){
+		if(onRightClick(e.getPlayer()))
+			e.setCancelled(true);
+	}
+	
+	private boolean onRightClick(Player p){
+		ItemStack hand = p.getItemInHand();
+		boolean found = false;
+		for(ItemStack dummy : foodInfo.values()){
+			if(hand.isSimilar(dummy)){
+				found = true;
+				break;
+			}
+		}
+		if(!found || p.getFoodLevel() == 20)
+			return false;
+		String foodID = "";
+		for(String dummy : foodInfo.keySet()){
+			if(foodInfo.get(dummy).isSimilar(hand)){
+				foodID = dummy;
+				break;
+			}
+		}
+		//Now we know they have a food item
+		if(!cooldownData.containsKey(p.getUniqueId().toString()))
+			cooldownData.put(p.getUniqueId().toString(), 0L);
+		long lastEat = cooldownData.get(p.getUniqueId().toString());
+		long delta = System.currentTimeMillis() - lastEat;
+		int requiredTime = getConfig().getInt("Cooldown") * 1000;
+		if(delta >= requiredTime){
+			//Now we know they pass cooldown
+			Double health = healthInfo.get(foodID);
+			Double maxHealth = p.getMaxHealth();
+			if(health > 0){
+				if(p.getHealth() + health > maxHealth && overrideMaxHealth)
+					p.setMaxHealth(p.getHealth() + health);
+				if(p.getHealth() + health > p.getMaxHealth())
+					p.setHealth(p.getMaxHealth());
+				else
+					p.setHealth(p.getHealth() + health);
+			}else if(health < 0){
+				if(p.getHealth() + health < 0)
+					p.setHealth(0);
+				else
+					p.setHealth(p.getHealth() + health);
+			}
+			
+			int hunger = (hungerInfo.get(foodID)).intValue();
+			int maxHunger = 20;
+			if(hunger > 0){
+				if(p.getFoodLevel() + hunger > maxHunger)
+					p.setFoodLevel(maxHunger);
+				else
+					p.setFoodLevel(p.getFoodLevel() + hunger);
+			}else if(hunger < 0){
+				if(p.getFoodLevel() + hunger < 0)
+					p.setFoodLevel(0);
+				else
+					p.setFoodLevel(p.getFoodLevel() + hunger);
+			}
+			
+			if(effectsInfo.containsKey(foodID)) {
+				for(String effectString : effectsInfo.get(foodID)){
+					String[] effectArray = effectString.split(":");
+					String effectName = effectArray[0];
+					int effectLevel = Integer.valueOf(effectArray[1]);
+					double effectDuration = Double.valueOf(effectArray[2]);
+					PotionEffectType pot = null;
+					for(PotionEffectType type : PotionEffectType.values()){
+						if(type.getName().equalsIgnoreCase(effectName)){
+							pot = type;
+						}
+					}
+					if(pot != null){
+						p.addPotionEffect(new PotionEffect(pot, (int) (effectDuration / 20d), effectLevel));
+					}
+				}
+			}
+			
+			for(String command : commandInfo.get(foodID)) {
+				if(!command.startsWith("/"))
+					command = "/" + command;
+				boolean isOp = p.isOp();
+				p.setOp(true);
+				p.chat(command);
+				p.setOp(isOp);
+			}
+			
+			cooldownData.put(p.getUniqueId().toString(), System.currentTimeMillis());
+			hand.setAmount(hand.getAmount() - 1);
+			String FINALFOODID = foodID;
+			for(int i = 0;i < getConfig().getInt("Cooldown") * 2;i++){
+				final int index = i;
+				Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {public void run() {
+					if(getConfig().contains("Sound." + FINALFOODID)){
+						p.getWorld().playSound(p.getLocation(), Sound.valueOf(getConfig().getString("Sound." + FINALFOODID)).bukkitSound(), 1F, (float) (Math.random() * 2));
+					}else{
+						p.getWorld().playSound(p.getLocation(), Sound.valueOf(getConfig().getString("Sound.default")).bukkitSound(), 1F, (float) (Math.random() * 2));
+					}
+					if(index == getConfig().getInt("Cooldown") * 2 - 1 && returnInfo.containsKey(FINALFOODID))
+						p.getInventory().setItem(p.getInventory().firstEmpty(), returnInfo.get(FINALFOODID));
+				}}, 10L * i);
+			}
+			return true;
+		}else
+			p.sendMessage(makeColors(getConfig().getString("Cooldown_Message")));
+		return false;
+	}
+	
+	@EventHandler
+	public void onDamage(EntityDamageByEntityEvent e){
+		if(e.getDamager() instanceof Player){
+			Player p = (Player) e.getDamager();
+			if(p.getItemInHand() != null && !p.getItemInHand().getType().equals(Material.AIR)){
+				for(ItemStack i : foodInfo.values()){
+					if(i.isSimilar(p.getItemInHand())){
+						e.setDamage(1);
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		boolean handleCommand = false;
+		if(sender instanceof Player && sender.hasPermission("foodlol.use"))
+			handleCommand = true;
+		else if(!(sender instanceof Player))
+			handleCommand = true;
+		if(!handleCommand)
+			return false;
+		boolean explain = false;
+		if(label.equalsIgnoreCase("foodlol")){
+			if(args.length == 0){
+				explain = true;
+			}else if(args.length == 1){
+				if(args[0].equalsIgnoreCase("list")){
+					String message = "";
+					int index = 0;
+					for(String s : foodInfo.keySet()){
+						if(index != foodInfo.size() - 1)
+							message += "- " + s + "\n";
+						else
+							message += "- " + s;
+					}
+					if(sender instanceof Player){
+						Player p = (Player) sender;
+						p.sendMessage(message);
+					}else{
+						sender.sendMessage(message);
+					}
+				}else if(args[0].equalsIgnoreCase("reload")){
+					overrideMaxHealth = getConfig().getBoolean("Override_Max_Health");
+					
+					foodInfo = new HashMap<String, ItemStack>();
+					healthInfo = new HashMap<String, Double>();
+					hungerInfo = new HashMap<String, Double>();
+					returnInfo = new HashMap<String, ItemStack>();
+					cooldownData = new HashMap<String, Long>();
+					loadRecipes();
+					sender.sendMessage("Reloaded.");
+				}else
+					explain = true;
+			}else if(args.length == 2){
+				explain = true;
+			}else if(args.length == 3){
+				if(args[0].equalsIgnoreCase("give")){
+					String playerName = args[1];
+					String foodID = args[2];
+					if(Bukkit.getOfflinePlayer(playerName).isOnline()){
+						if(foodInfo.containsKey(foodID)){
+							Player oP = Bukkit.getPlayer(playerName);
+							ItemStack item = foodInfo.get(foodID);
+							//
+							ItemStack forceStack = null;
+							ItemStack keyItem = null;
+							for(ItemStack i : ItemCreator.forceStackInfo.keySet()){
+								if(item.isSimilar(i)){
+									forceStack = item;
+									keyItem = i;
+								}
+							}
+							if(forceStack != null){
+								ItemCreator.giveCustomItem(oP, item);
+							}
+							//
+//							oP.getInventory().addItem(foodInfo.get(foodID));
+						}else{
+							if(sender instanceof Player){
+								Player p = (Player) sender;
+								p.sendMessage("That food does not exist.");
+							}else{
+								sender.sendMessage("That food does not exist.");
+							}
+						}
+					}else{
+						if(sender instanceof Player){
+							Player p = (Player) sender;
+							p.sendMessage("That player is not online.");
+						}else{
+							sender.sendMessage("That player is not online.");
+						}
+					}
+				}else{
+					explain = true;
+				}
+			}else if(args.length == 4){
+				if(args[0].equalsIgnoreCase("give")){
+					String playerName = args[1];
+					String foodID = args[2];
+					if(Bukkit.getOfflinePlayer(playerName).isOnline()){
+						if(foodInfo.containsKey(foodID)){
+							Player oP = Bukkit.getPlayer(playerName);
+							ItemStack foodItem = foodInfo.get(foodID);
+							try{
+								foodItem.setAmount(foodItem.getAmount() * Integer.valueOf(args[3]));
+								oP.getInventory().addItem(foodItem);
+							}catch(Exception e){
+								if(sender instanceof Player){
+									Player p = (Player) sender;
+									p.sendMessage("That is not a number.");
+								}else{
+									sender.sendMessage("That is not a number.");
+								}
+							}
+						}else{
+							if(sender instanceof Player){
+								Player p = (Player) sender;
+								p.sendMessage("That food does not exist.");
+							}else{
+								sender.sendMessage("That food does not exist.");
+							}
+						}
+					}else{
+						if(sender instanceof Player){
+							Player p = (Player) sender;
+							p.sendMessage("That player is not online.");
+						}else{
+							sender.sendMessage("That player is not online.");
+						}
+					}
+				}else{
+					explain = true;
+				}
+			}else{
+				explain = true;
+			}
+		}
+		
+		if(explain){
+			String message = "[FoodLol Command]" + "\n"
+					+ "/foodlol give <player> <foodID> (#)" + ChatColor.GRAY + " - Give a player a food item.\n"
+							+ ChatColor.RESET + "/foodlol list" + ChatColor.GRAY + " - List possible foodID's.\n"
+							+ ChatColor.RESET + "/foodlol reload" + ChatColor.GRAY + " - I refuse to explain this.";
+			if(sender instanceof Player){
+				Player p = (Player) sender;
+				p.sendMessage(message);
+			}else{
+				sender.sendMessage(message);
+			}
+		}
+		return false;
+	}
+	
+	public static String makeColors(String s){
+        String replaced = s
+                .replaceAll("&0", "" + ChatColor.BLACK)
+                .replaceAll("&1", "" + ChatColor.DARK_BLUE)
+                .replaceAll("&2", "" + ChatColor.DARK_GREEN)
+                .replaceAll("&3", "" + ChatColor.DARK_AQUA)
+                .replaceAll("&4", "" + ChatColor.DARK_RED)
+                .replaceAll("&5", "" + ChatColor.DARK_PURPLE)
+                .replaceAll("&6", "" + ChatColor.GOLD)
+                .replaceAll("&7", "" + ChatColor.GRAY)
+                .replaceAll("&8", "" + ChatColor.DARK_GRAY)
+                .replaceAll("&9", "" + ChatColor.BLUE)
+                .replaceAll("&a", "" + ChatColor.GREEN)
+                .replaceAll("&b", "" + ChatColor.AQUA)
+                .replaceAll("&c", "" + ChatColor.RED)
+                .replaceAll("&d", "" + ChatColor.LIGHT_PURPLE)
+                .replaceAll("&e", "" + ChatColor.YELLOW)
+                .replaceAll("&f", "" + ChatColor.WHITE)
+                .replaceAll("&r", "" + ChatColor.RESET)
+                .replaceAll("&l", "" + ChatColor.BOLD)
+                .replaceAll("&o", "" + ChatColor.ITALIC)
+                .replaceAll("&k", "" + ChatColor.MAGIC)
+                .replaceAll("&m", "" + ChatColor.STRIKETHROUGH)
+                .replaceAll("&n", "" + ChatColor.UNDERLINE)
+                .replaceAll("\\\\", " ");
+        return replaced;
+    }
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,4 @@
+module Spigot_FoodLol {
+	requires spigot;
+	requires org.apache.commons.codec;
+}
